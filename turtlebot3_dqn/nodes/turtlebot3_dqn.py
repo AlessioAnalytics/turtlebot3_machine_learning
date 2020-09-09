84e63f0e7122dee1e7161fe92864cd72c4ee51ff
tnfru <lars.lmueller@gmail.com>
2020-09-09 20:45:12 +0200

    add: authors in compliance with licence

--- turtlebot3_dqn/nodes/turtlebot3_dqn.py
+++ turtlebot3_dqn/nodes/turtlebot3_dqn.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 #################################################################################
 
-# Authors: Gilbert #
+# Authors: Gilbert , Widowski, Müller#
 import argparse
 
 import rospy
