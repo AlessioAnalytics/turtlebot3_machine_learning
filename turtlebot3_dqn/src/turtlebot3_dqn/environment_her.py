93b9900e80cafcbdd7e217e4ea0026980ce6335e
luwi1993 <lwidowski@alessio-analytics.com>
2020-09-10 11:52:34 +0200

    add authors to license

--- turtlebot3_dqn/src/turtlebot3_dqn/environment_her.py
+++ turtlebot3_dqn/src/turtlebot3_dqn/environment_her.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 #################################################################################
 
-# Authors: Gilbert #
+# Authors: Gilbert, Widowski, Müller#
 
 import rospy
 import numpy as np
